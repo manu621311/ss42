667a9989272517aee34cf93d8f79ac2e2ecd991f
tars <ajaysharma2ku@gmail.com>
2020-03-24 15:59:38 +0530

    Added google authentication

--- ss/api/views.py
+++ ss/api/views.py
@@ -12,6 +12,16 @@ from social_core.backends.oauth import BaseOAuth2
 from django.contrib.auth import authenticate, login
 from rest_framework_jwt.settings import api_settings
 
+# google log in imports
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
+from rest_framework.utils import json
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import requests
+from rest_framework_jwt.settings import api_settings
+from django.contrib.auth.models import User
+
 
 from social_core.exceptions import MissingBackend, AuthTokenError, AuthForbidden
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -85,3 +95,35 @@ class SocialLoginView(generics.GenericAPIView):
                 "token": data.get('token')
             }
             return Response(status=status.HTTP_200_OK, data=response)
+
+class GoogleView(APIView):
+    def post(self, request):
+        payload = {'access_token': request.data.get("token")}  # validate the token
+        r = requests.get('https://www.googleapis.com/oauth2/v2/userinfo', params=payload)
+        data = json.loads(r.text)
+
+        if 'error' in data:
+            content = {'message': 'wrong google token / this google token is already expired.'}
+            return Response(content)
+
+        # create user if not exist
+        try:
+            user = User.objects.get(email=data['email'])
+        except User.DoesNotExist:
+            user = User()
+            user.username = data['email']
+            # provider random default password
+            user.password = make_password(BaseUserManager().make_random_password())
+            user.email = data['email']
+            user.save()
+
+        # token = RefreshToken.for_user(user)  # generate token without username & password
+
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        response = {}
+        response['username'] = user.username
+        response['access_token'] = str(token)
+        # response['refresh_token'] = str(token)
+        return Response(response)
