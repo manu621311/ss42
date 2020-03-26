1549541439108c9739b0278d0d21ff9ec1b7516a
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-03-26 18:37:04 +0530

    working ione

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -26,7 +26,7 @@ from taggit_serializer.serializers import (TagListSerializerField,
 #         request=self.context.get("request")
 #         return instance.commentss.filter(pk=request.user.pk).exists()
 class Spost(TaggitSerializer,serializers.ModelSerializer):
-    tags = TagListSerializerField(required=False)
+    tags = TagListSerializerField()
 
     author=serializers.StringRelatedField(read_only=True)
     # likes_count=serializers.SerializerMethodField(read_only=True)
