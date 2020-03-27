58124d841adcc9ab4297e9dc6b7fcdb71e9dadba
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-03-27 22:45:45 +0530

    changed csrf

--- ss/settings.py
+++ ss/settings.py
@@ -130,17 +130,17 @@ WSGI_APPLICATION = 'ss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'scrapshut',
-#          'USER': 'db_user',
-#          'PASSWORD': 'pass',
-#          'HOST': 'localhost',
-#          'PORT': '',
-#      }
-#  }
+#
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'scrapshut',
+         'USER': 'db_user',
+         'PASSWORD': 'pass',
+         'HOST': 'localhost',
+         'PORT': '',
+     }
+ }
 
 #
 
@@ -151,16 +151,16 @@ WSGI_APPLICATION = 'ss.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'mynameisFB1987',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
