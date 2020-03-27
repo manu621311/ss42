a268fda41e403a04ee740da6bc76093498a517af
abhishekraj272 <abhishekraj272@gmail.com>
2020-03-27 18:06:03 +0530

    added user api to get all posts

--- ss/settings.py
+++ ss/settings.py
@@ -131,23 +131,23 @@ WSGI_APPLICATION = 'ss.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'scrapshut',
-#         'USER': 'db_user',
-#         'PASSWORD': 'pass',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scrapshut',
+        'USER': 'db_user',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
