61e48d2ce1023797b83d4c3d4bbfcae5a7ba413f
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-03-27 19:28:19 +0530

    changed pagination

--- ss/settings.py
+++ ss/settings.py
@@ -285,7 +285,7 @@ REST_FRAMEWORK={
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 200,
+    'PAGE_SIZE': 100,
 }
 # OAUTH2_PROVIDER = {
 #     'OAUTH2_BACKEND_CLASS': 'oauth2_provider.oauth2_backends.JSONOAuthLibCore',
