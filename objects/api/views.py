9e319951cf39da40d52c953ee6edf723ef81ca6f
tars <ajaysharma2ku@gmail.com>
2020-04-12 20:20:53 +0530

    Added profile tags

--- objects/api/views.py
+++ objects/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .serializers import UserDetailSerializer
+from .serializers import UserDetailSerializer, ProfileSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser,AllowAny
 from rest_framework import mixins, generics
@@ -11,7 +11,11 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from posts.models import Post
+from objects.models import Profile
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework import status
+from posts.api.permissions import IsAuthorOrReadOnly
 
 
 class PostListUser(generics.ListAPIView, APIView):
@@ -22,3 +26,35 @@ class PostListUser(generics.ListAPIView, APIView):
     def get_queryset(self):
 
         return Post.objects.filter(author=self.request.user)
+
+class ProfileTags(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes =[IsAuthenticatedOrReadOnly,IsAuthorOrReadOnly]
+    authentication_classes =(TokenAuthentication,JSONWebTokenAuthentication)
+
+
+    def create(self, request):
+        url = request.data.get('url')
+        user = str(request.user)
+        user_exist = Profile.objects.filter(username=self.request.user.username)
+        already_exists = False
+        if len(user_exist) == 0:
+            already_exists = False
+        else:
+            already_exists = True
+        if already_exists:
+            return Response({"details" : "Tags for this user already exists !"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return super(ProfileTags, self).create(request)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = Profile.objects.get(username=self.request.user.username)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_create(self, serializer):
+        serializer.save(username = self.request.user.username)
--- objects/models.py
+++ objects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    username = models.CharField(max_length=30, blank=True)
+    tags = TaggableManager(blank=True)
+
+    def __str__(self):
+        return self.username
