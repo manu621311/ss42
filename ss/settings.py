4fe6fd0b2c625f66a8b10e9096231856c3298abe
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-04-24 16:34:37 +0530

    added users

--- ss/settings.py
+++ ss/settings.py
@@ -135,16 +135,16 @@ WSGI_APPLICATION = 'ss.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 #
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'scrapshut',
-#          'USER': 'db_user',
-#          'PASSWORD': 'pass',
-#          'HOST': 'localhost',
-#          'PORT': '',
-#      }
-#  }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'scrapshut',
+         'USER': 'db_user',
+         'PASSWORD': 'pass',
+         'HOST': 'localhost',
+         'PORT': '',
+     }
+ }
 
 #
 
@@ -155,16 +155,16 @@ WSGI_APPLICATION = 'ss.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'mynameisFB1987',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # CHANNEL_LAYERS = {
 #     "default": {
