0a0c111199d5503eb8cfc7b524177e7e78ac290a
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-05-10 02:42:26 +0530

    objects

--- objects/models.py
+++ objects/models.py
@@ -6,6 +6,6 @@ from taggit.managers import TaggableManager
 class Profile(models.Model):
     username = models.CharField(max_length=30, blank=True)
     tags = TaggableManager(blank=True)
-    Scrapcoins = models.PositiveIntegerField()
+    Scrapcoins = models.PositiveIntegerField(default=0)
     def __str__(self):
         return self.username
