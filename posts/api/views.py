e28630a02b36317bd2a0514826cda712d69a7eb1
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-06-01 03:43:07 +0530

    serch

--- posts/api/views.py
+++ posts/api/views.py
@@ -422,7 +422,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 class ForServicePostViewSet(viewsets.ModelViewSet):
     close_old_connections()
     queryset = Post.objects.all()
-    search_fields = ['url','tags','review']
+    search_fields = ('url','tags','review')
     serializer_class = PostSerializer_read
     http_method_names = ['get']
     filter_backends = (filters.SearchFilter,)
