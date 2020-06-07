7fd8d1eb582c14bdd2b8f7e4922e23ebd84a0105
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2020-06-08 01:22:41 +0530

    company specific host

--- ss/settings.py
+++ ss/settings.py
@@ -154,7 +154,7 @@ DATABASES = {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': 'scrapshut',
            'USER': 'postgres',
-           'PASSWORD': 'ajay',
+           'PASSWORD': 'pass',
            'HOST': 'localhost',
            'PORT': '',
 
