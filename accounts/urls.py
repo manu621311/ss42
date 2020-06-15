64cae2dc6896d01abadad2d5be626d2948deae83
Abhishek Raj <abhishekraj272@gmail.com>
2020-06-15 23:29:58 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
-from .views import create_verification
+from .views import create_verification, confirm_verification
 from django.urls import path
 
 urlpatterns = [
-    path('create/<int:id>', create_verification)
+    path('create/<int:id>', create_verification),
+    path('confirm/<str:complex>/<int:id>/<int:otp>', confirm_verification),
 ]
